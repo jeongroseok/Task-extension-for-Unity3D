bca5c344e81cdf03e603a2dfd615ef52058b32c2
Seok Jeong-ro <jeongroseok@gmail.com>
2016-09-23 18:30:34 +0900

    upload

---
+++ Assets/Parse/Tasks/UnityTaskScheduler.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace System.Threading.Tasks
+{
+    public class UnityTaskScheduler : MonoBehaviour
+    {
+        private static UnityTaskScheduler instance;
+        private static object syncRoot = new Object();
+        private List<Action> actions = new List<Action>();
+        private List<Action> excutingActions = new List<Action>();
+
+        public static UnityTaskScheduler Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    throw new Exception("not initialized!");
+                }
+                return instance;
+            }
+        }
+
+        public static void Initialize()
+        {
+            if (instance == null)
+            {
+                lock (syncRoot)
+                {
+                    var go = new GameObject("_Task");
+                    instance = go.AddComponent<UnityTaskScheduler>();
+                }
+            }
+        }
+
+        private IEnumerator Start()
+        {
+            while (true)
+            {
+                lock (syncRoot)
+                {
+                    excutingActions.Clear();
+                    excutingActions.AddRange(actions);
+                    actions.Clear();
+                }
+                foreach (var action in excutingActions)
+                {
+                    action();
+                }
+                yield return null;
+            }
+        }
+
+        public void Post(IEnumerator coroutine)
+        {
+            Post(() => StartCoroutine(coroutine));
+        }
+
+        public void Post(Action a)
+        {
+            lock (syncRoot)
+            {
+                actions.Add(a);
+            }
+        }
+    }
+}
---
+++ Assets/TaskTest.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading.Tasks;
+
+public class TaskTest : MonoBehaviour
+{
+
+    private IEnumerator Start()
+    {
+        UnityTaskScheduler.Initialize();
+
+        var task = Task.Run(() => print("First action."))
+            .ContinueWith(t => print("wait for click"))
+            .ContinueWith(WaitForClick())
+            .ContinueWith(t =>
+            {
+                print("Second action.");
+            })
+            .ContinueWith(new WaitForSeconds(1))
+            .ContinueWith(t =>
+            {
+                print("Third action.");
+            });
+        yield break;
+    }
+
+    private IEnumerator WaitForClick()
+    {
+        while (!clicked)
+        {
+            yield return null;
+        }
+    }
+
+    private bool clicked = false;
+    private void OnGUI()
+    {
+        if (GUILayout.Button("Click!"))
+        {
+            clicked = true;
+        }
+    }
+}
